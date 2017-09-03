ccd2df59024eb19544f441e8de0e7c1fdfa052e8
M-Lewinski <lewin.michal@gmail.com>
2017-09-03 22:02:59 +0200

    Finished character animation, fixed problems with transition and character no longer stick to vertical walls

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -4,30 +4,68 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour {
 
-    public float maxSpeed = 10f;
+    /// Variables related with moving horizontaly
+    public float groundSpeedX = 10f;
+    public float flyingSpeedX = 0.9f;
     bool facingRight = true;
 
-    Animator anim;
+    /// Variables related with jumping and falling
+    public bool grounded = true; // Check if character is on the ground
+    public Transform groundCheck; // Where is ground compare to character position
+    private float groundRadius = 0.2f; // Circle radius
+    public LayerMask whatIsGround; // On which objects our character can land on
+    public float jumpForce = 4f;
 
-	/// Use this for initialization
+    Animator anim;
+    private Rigidbody2D rigidbody2D;
+
 	void Start () {
         anim = GetComponent<Animator>();
-	}
-	
-	/// Update is called once per frame
-	void FixedUpdate () {
-        float move = Input.GetAxis("Horizontal");
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
+    void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("Ground",false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));            
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame, the time between calles is fixed
+    /// </summary>
+    void FixedUpdate ()
+    {
+        MoveY();
+        MoveX();
+    }
+
+    private void MoveY()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("speedY", rigidbody2D.velocity.y);
+    }
+
+    private void MoveX()
+    {
+        float move = Input.GetAxis("Horizontal"); // Get intputs
+        if (!grounded) move *= flyingSpeedX;
         anim.SetFloat("Speed", Mathf.Abs(move));
 
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        rigidbody2D.velocity = new Vector2(move * groundSpeedX, GetComponent<Rigidbody2D>().velocity.y);
 
-        if (move > 0 && !facingRight)
+        if (move > 0 && !facingRight) // Check where CharacterTransform face and in which direction it's moving
             Flip();
         else if (move < 0 && facingRight)
             Flip();
-	}
+    }
 
+    /**
+     * 
+     */
     void Flip()
     {
         facingRight = !facingRight;
