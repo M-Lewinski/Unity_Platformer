35dc76149464e69d0f43ea97821346ed30ef43ed
M-Lewinski <lewin.michal@gmail.com>
2017-09-04 11:40:01 +0200

    Add random brush

---
+++ Assets/Editor/WallBrush.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallBrush : GridBrush
+{
+
+    public Sprite preview;
+    public Sprite[] randomSprites;
+
+    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        Tilemap tilemap = gridLayout.GetComponent<Tilemap>();
+        int randomTile = UnityEngine.Random.Range(0, randomSprites.Length);
+        tilemap.SetTile(position,randomSprites[randomTile]);
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
