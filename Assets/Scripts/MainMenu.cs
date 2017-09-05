a4b3e9422441dd39cd1d2f48a56250d0cc4b3d2e
pawelacio <pawel.ad.kaczmarek@student.put.poznan.pl>
2017-09-05 12:13:46 +0200

    Adding Main Menu to the game and checkpoint flag

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+    public string optionsScene;
+
+
+
+    public void NewGame()
+    {
+        Application.LoadLevel(startLevel);
+    }
+
+    public void GameOptions()
+    {
+        Debug.Log("GameOptions selected");
+        //Here will be game options
+        //Application.LoadLevel(optionsScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
