097bcea770c51ff623daf51a16e8aed006dbf819
M-Lewinski <lewin.michal@gmail.com>
2017-09-19 18:22:24 +0200

    small changes to cursor

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -17,12 +17,14 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(isPaused)
-        {
+		{
+		    Cursor.visible = true;
             pauseMenuCanvas.SetActive(true);
             Time.timeScale = 0f;
         }
         else
-        {
+		{
+		    Cursor.visible = false;
             pauseMenuCanvas.SetActive(false);
             Time.timeScale = 1f;
         }
